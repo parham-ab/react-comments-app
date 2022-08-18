73731e3735d75e1862e233326836230f1c4994cb
Parham Abolghasemi <parhamab17@gmail.com>
2022-08-18 10:57:58 +0430

    bugs fixed, post data to server & get data from server, error handling

--- src/components/CommentsList.js
+++ src/components/CommentsList.js
@@ -1,10 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 // mui
 import { Button, TextField, Typography, Grid, Container } from "@mui/material";
 // icons
 import SendIcon from "@mui/icons-material/Send";
+// formik
+import { Formik, Form, Field, ErrorMessage, FieldArray } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+import CustomError from "./CustomError";
+
+// initial values
+const initialValues = {
+  name: "",
+  email: "",
+  comment: "",
+};
+// validate function
+const validationSchema = Yup.object({
+  name: Yup.string()
+    .trim()
+    .required("Required!")
+    .max(50, "your name can't be more than 50 chars!"),
+  email: Yup.string()
+    .matches(
+      /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,3}$/i,
+      "invalid Email address"
+    )
+    .required("Required"),
+  comment: Yup.string()
+    .trim()
+    .min(5, "your comment must be at least 5 or more chars!")
+    .max(500, "your comment can't be more than 500 chars!")
+    .required("Text Required!"),
+});
 
 const CommentsList = () => {
+  const [mainData, setMainData] = useState([]);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  // submit
+  const onSubmit = (values) => {
+    setMainData(values);
+    const postComments = async () => {
+      try {
+        await axios.post("https://fakestoreapi.com/products", values);
+        const { data } = await axios.get("https://fakestoreapi.com/products");
+        console.log(data);
+      } catch (err) {
+        setError(err);
+        console.log(error.message);
+      }
+    };
+    postComments();
+  };
+
   return (
     <Container maxWidth="lg">
       <Grid
@@ -23,42 +72,50 @@ const CommentsList = () => {
         </Grid>
 
         <Grid item xs={12} mb={5}>
-          <TextField
-            sx={{ mt: 3 }}
-            id="name"
-            label="Name"
-            name="name"
-            // helperText="Some important text"
-            variant="standard"
-            fullWidth
-            size="small"
-          />
-          <TextField
-            sx={{ mt: 3 }}
-            id="email"
-            label="Email"
-            name="email"
-            // helperText="Some important text"
-            variant="standard"
-            fullWidth
-            size="small"
-          />
-          <TextField
-            sx={{ mt: 3 }}
-            id="comment"
-            label="Comment"
-            name="comment"
-            // helperText="Some important text"
-            variant="standard"
-            fullWidth
-            size="small"
-            multiline
-            rows={4}
-          />
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={onSubmit}
+          >
+            <Form>
+              <div>
+                <div>
+                  <label htmlFor="name">Name</label>
+                </div>
+                <Field type="text" name="name" placeholder="Name" />
+                <ErrorMessage name="name" component={CustomError} />
+              </div>
+
+              <div>
+                <div>
+                  <label htmlFor="email">Email</label>
+                </div>
+                <Field type="text" name="email" placeholder="Email" />
+                <ErrorMessage name="email" component={CustomError} />
+              </div>
+
+              <div>
+                <div>
+                  <label htmlFor="comment">Comment</label>
+                </div>
+                <Field type="text" name="comment" placeholder="Comment" />
+                <ErrorMessage name="comment" component={CustomError} />
+              </div>
+
+              <Button
+                variant="contained"
+                size="small"
+                endIcon={<SendIcon />}
+                sx={{ mt: 5 }}
+                type="submit"
+                //   endIcon={!loading ? <SendIcon /> : <HourglassTopIcon />}
+                //   disabled={formik.isValidating}
+              >
+                Post
+              </Button>
+            </Form>
+          </Formik>
         </Grid>
-        <Button variant="contained" size="small" endIcon={<SendIcon />}>
-          Post
-        </Button>
       </Grid>
     </Container>
   );
---
+++ src/components/CustomError.js
@@ -0,0 +1,4 @@
+const CustomError = ({ children }) => {
+  return <span style={{ color: "#e75353", fontSize: "14px" }}>{children}</span>;
+};
+export default CustomError;
