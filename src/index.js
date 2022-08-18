743bf0d67b1b221ab4060fe31982303d5ef82612
Parham Abolghasemi <parhamab17@gmail.com>
2022-08-18 09:02:08 +0430

    fonts applied

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 // styles
 import "./assets/styles/index.scss";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+const theme = createTheme({
+  typography: {
+    fontFamily: ["Quicksand"].join(","),
+    fontSize: 15,
+    fontWeightLight: 400,
+    fontWeightMedium: 500,
+    fontWeightRegular: 600,
+    fontWeightBold: 700,
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>
+);
