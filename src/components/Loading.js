55d163d4cd9a449995172f85e2c76c314889a6fd
Parham Abolghasemi <parhamab17@gmail.com>
2022-08-19 13:34:06 +0430

    preloader styling

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,10 +1,15 @@
-import React from "react";
+import { Box, Typography } from "@mui/material";
+// icon
+import HourglassTopIcon from "@mui/icons-material/HourglassTop";
 
 const Loading = () => {
   return (
-    <div>
-      <h1>Loading...</h1>
-    </div>
+    <Box component="div" display="flex" justifyContent="center" mt={5}>
+      <Typography variant="h3">
+        <HourglassTopIcon sx={{ fontSize: "45px", color: "#252a62" }} />
+        Loading...
+      </Typography>
+    </Box>
   );
 };
 
